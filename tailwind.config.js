3ee937f92862e8bbc71d81d69651cc6427d3b66a
WooSangWook <raincoat@kakao.com>
2024-06-26 16:51:19 +0900

    Feat: Add 여름성경학교 페이지

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        dongle: ["Dongle", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
