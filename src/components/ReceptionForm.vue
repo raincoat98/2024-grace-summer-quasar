7b20b02ebede076791342615e1be799de75703d9
고윤호 <yoonho_go@ip-192-168-1-4.ap-northeast-2.compute.internal>
2024-06-26 18:19:21 +0900

    feat: sms

--- src/components/ReceptionForm.vue
+++ src/components/ReceptionForm.vue
@@ -1,9 +1,13 @@
 <script lang="ts" setup>
-import { ref } from "vue";
+import { reactive, computed } from "vue";
 
-const user = ref({
+const user = reactive({
   name: "",
   grade: "",
+  pickup: "",
+});
+const body = computed(() =>{
+  return `sms:01023318579?body=${user.name}/${user.grade}/${user.pickup}`;
 });
 </script>
 
@@ -12,7 +16,9 @@ const user = ref({
     <h1 class="font-dongle text-7xl">접수하기 🏖️</h1>
     <q-input filled v-model="user.name" label="이름" class="bg-white" />
     <q-input filled v-model="user.grade" label="학년" class="bg-white" />
-    <q-btn unelevated class="bg-green-500 font-bold py-3"> 신청하기 </q-btn>
+    <q-input filled v-model="user.pickup" label="탑승위치" class="bg-white" />
+    <q-btn unelevated class="bg-green-500 font-bold py-3"><a :href="body">안드로이드 신청하기</a></q-btn>
+    <q-btn unelevated class="bg-blue-500 font-bold py-3"><a :href="body">아이폰 신청하기</a></q-btn>
   </div>
 </template>
 
