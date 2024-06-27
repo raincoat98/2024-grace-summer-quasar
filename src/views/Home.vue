d438677219375b08a158ef0b063c6d2c0a005f34
WooSangWook <raincoat@kakao.com>
2024-06-27 15:05:06 +0900

    Feat: 지도 공유 추가

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,15 @@
 <script lang="ts" setup>
 import ReceptionForm from '@/components/ReceptionForm.vue';
 import ShareButtons from '@/components/ShareButtons.vue';
+
+function openNaverMap() {
+  const naverMapUrl = `https://naver.me/5chEWenv`;
+  window.open(naverMapUrl, '_blank');
+}
+function openKakaoMap() {
+  const kakaoMapUrl = `https://kko.to/Oau1Tshxg-`;
+  window.open(kakaoMapUrl, '_blank');
+}
 </script>
 
 <template>
@@ -37,11 +46,13 @@ import ShareButtons from '@/components/ShareButtons.vue';
         label="네이버 지도에서 보기"
         class="border-none bg-green-500 font-bold"
         rounded
+        @click="openNaverMap"
       />
       <q-btn
         class="text-black border-none bg-yellow-400 font-bold"
         rounded
         label="카카오 지도에서 보기"
+        @click="openKakaoMap"
       />
     </section>
     <section class="flex flex-col gap-2 font-dongle">
