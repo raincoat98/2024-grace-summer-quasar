2c41046754b1f0f38793ffa33f71c37efc9c9538
WooSangWook <raincoat@kakao.com>
2024-06-27 16:38:40 +0900

    Refactoring: title 컴포넌트 분리

---
+++ src/components/Title.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  titleContent: {
+    year: number;
+    title: string;
+  };
+}>();
+
+const title = computed(() => {
+  return `${props.titleContent.year} ${props.titleContent.title}`;
+});
+</script>
+
+<template>
+  <section
+    class="text-wrap break-keep font-dongle text-8xl leading-normal text-black-800"
+  >
+    {{ title }}
+  </section>
+</template>
+
+<style lang="scss" scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,21 @@
 <script lang="ts" setup>
-import ReceptionForm from '@/components/ReceptionForm.vue';
-import ShareButtons from '@/components/ShareButtons.vue';
+import Title from '@/components/Title.vue';
+import Timetable from '@/components/Timetable.vue';
+import Programs from '@/components/Programs.vue';
 import AboutUs from '@/components/AboutUs.vue';
 import BibleVerses from '@/components/BibleVerses.vue';
-import Programs from '@/components/Programs.vue';
+import ReceptionForm from '@/components/ReceptionForm.vue';
+import ShareButtons from '@/components/ShareButtons.vue';
 
 const titleContent = ref({
   year: 2024,
   title: '여름성경학교 광주성전 ⛱️',
 });
-
-const title = computed(() => {
-  return `${titleContent.value.year} ${titleContent.value.title}`;
-});
 </script>
 
 <template>
   <div class="flex flex-col gap-10 text-center lg:gap-20">
-    <section
-      class="text-wrap break-keep font-dongle text-8xl leading-normal text-black-800"
-    >
-      {{ title }}
-    </section>
+    <Title :titleContent="titleContent"> </Title>
     <Timetable />
     <Programs />
     <AboutUs />
